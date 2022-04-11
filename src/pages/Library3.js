df4749671d79d148253981660bc5061cdec08760
AbhijitPatil395 <47312163+AbhijitPatil395@users.noreply.github.com>
2022-04-12 00:46:46 +0530

    LibraryCommit

--- src/pages/Library3.js
+++ src/pages/Library3.js
@@ -18,13 +18,13 @@ function Library()
 
     useEffect(() => {
 
-        fetch("https://localhost:44378/api/products")
+        fetch("https://localhost:44378/api/products/Getproducts")
             .then(res => res.json())
             .then((result) => { setData(result); setFilteredData(result); setFilteredData2(result); });
-        fetch("https://localhost:44378/api/languages")
+        fetch("https://localhost:44378/api/languages/Getlanguages")
             .then(res => res.json())
             .then((result) =>{setLang(result)});
-        fetch("https://localhost:44378/api/generes")
+        fetch("https://localhost:44378/api/generes/Getgeneres")
             .then(res => res.json())
             .then((result) =>{setGenere(result)});
     }, [])
@@ -34,7 +34,7 @@ function Library()
         if (event.target.checked){
            
             setTran('r');
-            fetch("http://localhost:8080/crud/rentableproductsearch/1")
+            fetch("https://localhost:44378/api/products/GetproductRentable")
             .then(res => res.json())
             .then((result) => { setData(result); setFilteredData(result); setFilteredData2(result); });
            
@@ -42,7 +42,7 @@ function Library()
         else{
             
             setTran('p')
-            fetch("https://localhost:44378/api/products")
+            fetch("https://localhost:44378/api/products/Getproducts")
             .then(res => res.json())
             .then((result) => { setData(result); setFilteredData(result); setFilteredData2(result); });
             
@@ -55,30 +55,30 @@ function Library()
                 // console.log(val)
                 if(val==1)
                 {console.log("case1");
-                setFilteredData2(data.filter((elem) => elem.productLanguage=='1'));
-                setFilteredData(data.filter((elem) => elem.productLanguage=='1'));
+                setFilteredData2(data.filter((elem) => elem.product_language=='1'));
+                setFilteredData(data.filter((elem) => elem.product_language=='1'));
             
                  }
                 else if(val==2)
                 {console.log("case2");
-                setFilteredData2(data.filter((elem) => elem.productLanguage=='2'));
+                setFilteredData2(data.filter((elem) => elem.product_language=='2'));
                 setFilteredData(data.filter((elem) => elem.productLanguage=='2'));
              }
                 else if(val==4)
                 {console.log("case4");
                 setFilteredData2(data.filter((elem) => elem.productLanguage=='4')); 
-                setFilteredData(data.filter((elem) => elem.productLanguage=='4')); 
+                setFilteredData(data.filter((elem) => elem.product_language=='4')); 
             }
                 else if(val==3)
                 {console.log("case3");
-                setFilteredData2(data.filter((elem) => elem.productLanguage =='3'));
-                setFilteredData(data.filter((elem) => elem.productLanguage =='3'));
+                setFilteredData2(data.filter((elem) => elem.product_language =='3'));
+                setFilteredData(data.filter((elem) => elem.product_language =='3'));
             }
                 
                 else if(val==5)
                 {console.log("case5");
-                setFilteredData2(data.filter((elem) => elem.productLanguage =='5')); 
-                setFilteredData(data.filter((elem) => elem.productLanguage =='5')); 
+                setFilteredData2(data.filter((elem) => elem.product_language =='5')); 
+                setFilteredData(data.filter((elem) => elem.product_language =='5')); 
             }
                 else if(val==0)
                 {console.log("case0");
@@ -97,31 +97,31 @@ function Library()
         if(val==1)
         {
             console.log("case1");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='1'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='1'));}
         else if(val==2)
         {console.log("case2");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='2'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='2'));}
         else if(val==3)
         {console.log("case3");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='3'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='3'));}
         else if(val==4)
         {console.log("case4");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='4'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='4'));}
         else if(val==5)
         {console.log("case5");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='5'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='5'));}
         else if(val==6)
         {console.log("case6");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='6'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='6'));}
         else if(val==7)
         {console.log("case7");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='7'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='7'));}
         else if(val==8)
         {console.log("case8");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='8'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='8'));}
         else if(val==9)
         {console.log("case9");
-        setFilteredData(filteredData2.filter((elem) => elem.productGenere =='9'));}
+        setFilteredData(filteredData2.filter((elem) => elem.product_genere =='9'));}
         else if(val==0)
         {console.log("case0");
         setFilteredData(filteredData2);
@@ -159,7 +159,7 @@ function Library()
     const searchBook = (event) => {
         if(event.target.value=='')
         {
-            fetch("https://localhost:44378/api/products")
+            fetch("https://localhost:44378/api/products/Getproducts")
             .then(res => res.json())
             .then((result) => { 
                 setData(result); 
@@ -169,7 +169,7 @@ function Library()
         }
         else
         {
-            fetch("http://localhost:8080/product/searchbyname/"+event.target.value)
+            fetch("https://localhost:44378/api/products/GetproductByName/"+event.target.value)
             .then(res => res.json())
             .then((result) =>
              {
@@ -273,9 +273,9 @@ function Library()
                                             </Link>
                                             {tran=='p'?
                                             (<div style={{ paddingLeft: "0px" }} >
-                                                  <Card.Title><Button variant="primary" onClick={() => { submitHandler(book.productId) }} >Add to Cart</Button></Card.Title>
+                                                  <Card.Title><Button variant="primary" onClick={() => { submitHandler(book.product_id) }} >Add to Cart</Button></Card.Title>
                                               </div>):(<div className="mx-auto">
-                                              <Card.Title><Button variant="primary" href={"/Description/" + book.productId}>Rent</Button></Card.Title>
+                                              <Card.Title><Button variant="primary" href={"/Description/" + book.product_id}>Rent</Button></Card.Title>
                                               </div>)}
                                                 
                                         </Card.Body>
